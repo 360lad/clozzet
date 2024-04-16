dddbcdc02a7104ce48c335a15548d957139bdab0
L.A360 <lat.tif8605@gmail.com>
2024-04-16 12:28:50 +0300

    the add to cart textcontent was made to function that when one clicks it changes to remove  from cart

--- src/components/Shopproduct.jsx
+++ src/components/Shopproduct.jsx
@@ -1,34 +1,36 @@
-// import jeans from "../assets/images/jeans.png"
+function Shopproduct({ img, title, price, cart, setCart }) {
+  const handleAddToCart = (e, product) => {
+    if (e.target.textContent.toLowerCase() === "add to cart") {
+        
+      setCart((prev) => [...prev, product]);
 
-
-function Shopproduct({img,title,price,cart,setCart}){
-    const handleAddToCart=(e,product)=>{
-if(e.target.textcontent.toLowerCase()==="add to cart"){
-    setCart(prev=>[...prev,product]) ;{
-        e.target.textcontent==="Remove from Cart"
+      e.target.textContent = "Remove from Cart"
 
     }
-}
+    else{
+        e.target.textContent="Add to Cart"
     }
-    return(
-<div className="flex flex-wrap m-[1em]">
-    
-    <div className=" group ">
-<div className="w-[250px] h-[300px] bg-[#f5f0f0] flex justify-center items-center object-contain p-[1em] m-[1em]">
-    <img className="object-contain" src={img} alt="" /></div>
-    <div>
-<button onClick={e=>handleAddToCart(e,{img,title,price})}  className="bg-[#45C9A1] rounded  p-[1em] text-[white] w-[80%] justify-center items-center hidden group-hover:block">Add to Cart</button>
-</div>
-<div>
-
-<p>{title}</p>
-<p>{price}</p>
-
-</div>
-
-</div>
+  };
+  return (
+    <div className="flex flex-wrap m-[1em]">
+      <div className=" group ">
+        <div className="w-[250px] h-[300px] bg-[#f5f0f0] flex justify-center items-center object-contain p-[1em] m-[1em]">
+          <img className="object-contain" src={img} alt="" />
+        </div>
+        <div>
+          <button
+            onClick={(e) => handleAddToCart(e, { img, title, price })}
+            className="bg-[#45C9A1] rounded  p-[1em] text-[white] w-[80%] justify-center items-center hidden group-hover:block"
+          >
+            Add to cart
+          </button>
+        </div>
+        <div>
+          <p>{title}</p>
+          <p>{price}</p>
+        </div>
+      </div>
     </div>
-
-    )
+  );
 }
-export default Shopproduct
+export default Shopproduct;
